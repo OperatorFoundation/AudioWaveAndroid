ea51b3c26671c11cd220e7cbb53dfc59fe407e4d
consuelita <adelita@operatorfoundation.org>
2025-05-06 18:14:18 -0500

    Create ParameterValidator.kt

---
+++ AudioWave/src/main/java/org/operatorfoundation/audiowave/utils/ParameterValidator.kt
@@ -0,0 +1,109 @@
+package org.operatorfoundation.audiowave.utils
+
+import timber.log.Timber
+import kotlin.math.min
+
+/**
+ * Utility class for validating audio processing parameters with clear error messages.
+ *
+ * This class provides methods to validate and constrain parameter values commonly used in audio processing.
+ *
+ * Example usage:
+ * ```
+ * // Validate gain parameter
+ * val validGain = ParameterValidator.validateRange(inputGain, 0f, 5f, "Gain")
+ * ```
+ */
+object ParameterValidator
+{
+    /**
+     * Validate that a value falls within a specified range, constraining if necessary
+     *
+     * @param value The value to validate
+     * @param min The minimum allowed value
+     * @param max The maximum allowed value
+     * @param paramName The name of the parameter (for logging)
+     * @return The validated value, constrained to the specific range
+     */
+    fun validateOrConstrainToRange(value: Float, min: Float, max: Float, paramName: String): Float
+    {
+        val limitedValue = AudioUtils.limit(value, min, max)
+        if (limitedValue != value) {
+            Timber.w("$paramName value $value outside valid range [$min, $max], using $limitedValue instead")
+        }
+        return limitedValue
+    }
+
+    /**
+     * Validate that a value is non-negative, constraining it to zero if necessary.
+     *
+     * @param value The value to validate
+     * @param paramName The name of the parameter (for logging)
+     * @return The validated value, set to zero if it was negative
+     */
+    fun validateOrConstrainNonNegative(value: Float, paramName: String): Float
+    {
+        return validateOrConstrainToRange(value, 0f, Float.MAX_VALUE, paramName)
+    }
+
+    /**
+     * Validate that a value is positive (greater than zero), constraining it if necessary.
+     *
+     * @param value The value to validate
+     * @param paramName The name of the parameter (for logging)
+     * @return The validated value, set to a small positive value if it was zero or negative
+     */
+    fun validateOrConstrainToPositive(value: Float, paramName: String): Float
+    {
+        val minValue = 0.00001f
+        return validateOrConstrainToRange(value, minValue, Float.MAX_VALUE, paramName)
+    }
+
+    /**
+     * Validate that a value is between 0.0 and 1.0, constraining if necessary.
+     * Useful for parameters representing percentages or normalized values.
+     *
+     * @param value The value to validate
+     * @param paramName The name of the parameter (for logging)
+     * @return The validated value, constrained to the range [0.0, 1.0]
+     */
+    fun validateOrConstrainToNormalized(value: Float, paramName: String): Float
+    {
+        return validateOrConstrainToRange(value, 0f, 1f, paramName)
+    }
+
+    /**
+     * Validate that an array length matches an expected value.
+     *
+     * @param array The array to validate
+     * @param expectedLength The expected length of the array
+     * @param paramName The name of the parameter (for logging)
+     * @return True if the array length matches the expected length, false otherwise
+     */
+    fun validateArrayLength(array: Array<*>, expectedLength: Int, paramName: String): Boolean
+    {
+        val isValid = array.size == expectedLength
+        if (!isValid) {
+            Timber.w("$paramName has invalid length ${array.size}, expected $expectedLength")
+        }
+        return isValid
+    }
+
+    /**
+     * Validate that a parameter is one of the allowed values.
+     *
+     * @param value The value to validate
+     * @param allowedValues The set of allowed values
+     * @param paramName The name of the parameter (for logging)
+     * @param defaultValue The default value to use if the provided value is not allowed
+     * @return The validated value (original if valid, default if not)
+     */
+    fun <T> validateAllowedValue(value: T, allowedValues: Set<T>, paramName: String, defaultValue: T): T
+    {
+        if (value !in allowedValues) {
+            Timber.w("$paramName value $value is not allowed. Using default value $defaultValue")
+            return defaultValue
+        }
+        return value
+    }
+}
